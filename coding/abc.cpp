3797f39a29dae78324d7dcdf6f7b8cabfed61ca5
JG <jeje9893@gmail.com>
2024-04-19 13:34:29 +0900

    그냥 abc 테스트 중

--- coding/abc.cpp
+++ coding/abc.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 int main(){
+    int a = 1;
+    a += 1;
+    cout << a;
     cout << "abc\n";
-    cout << "abc\n";
-    cout << "abc\n";
-    cout << "abc\n";
-
     //이거 한줄 추가하기
 }
